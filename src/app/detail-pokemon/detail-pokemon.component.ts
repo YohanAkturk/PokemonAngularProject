0678d99768b93637e44cf527bcbbbd8229bb6f21
Yohan Akturk <56514@etu.he2b.be>
2024-01-09 11:41:27 +0100

    cloning

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pokemon-list';
 
@@ -12,7 +12,7 @@ export class DetailPokemonComponent implements OnInit {
   pokemonList: Pokemon[]
   pokemon: Pokemon|undefined
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.pokemonList = POKEMONS
@@ -21,4 +21,9 @@ export class DetailPokemonComponent implements OnInit {
       this.pokemon = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
     }
   }
+
+  goToPokemonList(){
+    this.router.navigate(['/pokemons'])
+  }
+
 }
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { POKEMONS } from '../mock-pokemon-list';
 import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -10,4 +11,9 @@ export class ListPokemonComponent {
 
   pokemonList: Pokemon[] = POKEMONS
 
+  constructor(private router: Router) {}
+
+  goToPokemon(pokemon: Pokemon){
+    this.router.navigate(['/pokemon', pokemon.id])
+  }
 }
